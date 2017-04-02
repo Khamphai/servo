af6074560b0bb8590ddc53f09c6ab340c48f8227
Khamphai <pphai67@gmail.com>
2017-04-02 23:34:10 +0700

    first commit

---
+++ servo.ino
@@ -0,0 +1,20 @@
+//servo.ino
+#include <Servo.h>
+ 
+Servo myservo;  
+ 
+int potpin = A0;   
+int val;          
+ 
+void setup()
+{
+  myservo.attach(9);
+}
+ 
+void loop()
+{
+  val = analogRead(potpin);            
+  val = map(val, 0, 1023, 0, 179);     
+  myservo.write(val);                       
+  delay(15);                                   
+}
